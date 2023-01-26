14a619d2d4c259824b773801210dfa9d291ac3ae
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-01-26 12:28:21 +0200

    use compact & findOrFail functions and remove route constraint

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $posts = Post::all();
 
-    return view('posts', ['posts' => $posts]);
+    return view('posts', compact('posts'));
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $post = Post::find($slug);
+    $post = Post::findOrFail($slug);
 
-    return view(
-        'post',
-        ['post' => $post]
-    );
-})->where('post', '[A-z_\-]+');
+    return view('post', compact('post'));
+});
