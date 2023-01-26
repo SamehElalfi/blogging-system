f9045844d30ac5f32b8164ea6e5cba2376314e45
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-01-26 12:27:24 +0200

    add findOrFail method and refactor find method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,7 +20,7 @@ class Post
     /**
      * Return all posts content from files at 'resources/posts'
      *
-     * @return String[]
+     * @return Collection
      */
     public static function all()
     {
@@ -30,24 +30,30 @@ class Post
         $files = File::files(resource_path('posts'));
 
         // get the content of every post file
-        $posts = collect($files)->map(function ($file) {
-            // `$file` is an object of all file information
-            // return $file->getContents();
+        $posts = collect($files)
+            ->map(function ($file) {
+                // parse post meta data and create new Post object for every post
+                return YamlFrontMatter::parseFile((string) $file);
+            })
 
-            // you can use `file_get_contents()` too
-            // we can get the file path by converting that object into string
-            // return file_get_contents((string) $file);
+            ->map(function ($document) {
+                // cache the post content in the memory for 5 seconds
+                // in real world app, you may want to set the cache for more than 5 seconds
+                // you may also use `now()->addSeconds(5)` instead of writing number of
+                // seconds. See https://laravel.com/docs/9.x/helpers#method-cache
+                // you may also want to use arrow function instead of anonymous function
+                $post = cache()->remember("posts.{$document->slug}", 5, function () use ($document) {
+                    return new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug
+                    );
+                });
 
-            // parse post meta data and create new Post object for every post
-            $document = YamlFrontMatter::parseFile((string) $file);
-            return new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            );
-        })->sortByDesc('date');
+                return $post;
+            })->sortByDesc('date');
 
         return $posts;
     }
@@ -61,11 +67,21 @@ class Post
      */
     public static function find($slug)
     {
-        // dangerous code don't use it in real-life app
-        // what if the `$slug` is malicious code
-        // its only for learning purposes
-        $path = resource_path("posts/{$slug}.html");
-        if (!file_exists($path)) {
+        return static::all()->firstWhere('slug', $slug);
+    }
+
+
+    /**
+     * Search for post file by its slug and return the content of the file if
+     * it's exist
+     *
+     * @param String $slug
+     * @return Post
+     */
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+        if (!$post) {
             throw new ModelNotFoundException();
 
             // no problem if we used `abort(404)` too` but its better to use
@@ -77,25 +93,6 @@ class Post
             // identifier (#debug)
             // return redirect('/');
         }
-
-        // cache the post content in the memory for 5 seconds
-        // in real world app, you may want to set the cache for more than 5 seconds
-        // you may also use `now()->addSeconds(5)` instead of writing number of
-        // seconds. See https://laravel.com/docs/9.x/helpers#method-cache
-        // you may also want to use arrow function instead of anonymous function
-        $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
-            var_dump('reading from file not from memory');
-
-            // parse post meta data and create new Post object
-            $document = YamlFrontMatter::parseFile($path);
-            return new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            );
-        });
 
         return $post;
     }
