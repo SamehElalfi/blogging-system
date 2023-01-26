662de95739abb8468be82fb68634500f011630ef
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-01-26 12:24:16 +0200

    use blade components and directives

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post</title>
-    <link rel="stylesheet" href="/app.css">
-
-</head>
-
-<body>
+<x-layout>
     <article>
-        <h2><?= $post->title ?></h2>
-        <datetime><?= gmdate('d M Y', $post->date) ?></datetime>
-        <?= $post->body ?>
+        <h2>{{ $post->title }}</h2>
+        <datetime>{{ gmdate('d M Y', $post->date) }}</datetime>
+        {!! $post->body !!}
     </article>
 
     <a href="/">Go Back</a>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
-    <?php foreach ($posts as $post): ?>
-    <article>
-        <h2><a href="/posts/<?= $post->slug ?>"><?= $post->title ?></a></h2>
-        <?= $post->excerpt ?>
-    </article>
-    <?php endforeach; ?>
-</body>
-
-</html>
+<x-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+            {{ $post->excerpt }}
+        </article>
+    @endforeach
+</x-layout>
