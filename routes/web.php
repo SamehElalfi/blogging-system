5ba6c0d374ab4d2067c6762756ec920eb81501e8
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-01-29 17:25:59 +0200

    use id instead of slug. this is coz that's the default in Eloquent model

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::get('/', function () {
     return view('posts', compact('posts'));
 });
 
-Route::get('posts/{post}', function ($slug) {
-    $post = Post::findOrFail($slug);
+Route::get('posts/{post}', function ($id) {
+    $post = Post::findOrFail($id);
 
     return view('post', compact('post'));
 });
