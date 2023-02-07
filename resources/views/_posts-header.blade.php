ad203ba45b272d9f5eb59c5d67f5d477db985de8
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-07 17:02:11 +0200

    remove author name from posts header

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -3,9 +3,7 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="mt-2 inline-flex">By Lary Laracore <img src="/images/lary-head.svg" alt="Head of Lary the mascot"></h2>
-
-    <p class="mt-14 text-sm">
+    <p class="mt-4 text-sm">
         Another year. Another update. We're refreshing the popular Laravel series with new content.
         I'm going to keep you guys up to speed with what's going on!
     </p>
