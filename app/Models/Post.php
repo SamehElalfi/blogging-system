b78e563cd57b9fa4106ebb5b51f8ac0f50f3def4
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-07 17:27:37 +0200

    add a scope to Post model to search among posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,17 @@ class Post extends Model
 
     protected $fillable = ['title', 'excerpt', 'body',];
 
+    public function scopeFilter($query, array $filters = [])
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+
+        return $query;
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
