4443716d9bff6877aa5274275390ba4ec3cca806
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-08 22:46:49 +0200

    fix: rename password confirmation field to match laravel validation rules

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -41,9 +41,9 @@
                                 placeholder="Password">
                         </div>
                         <div>
-                            <label for="confirm-password" class="sr-only">Password</label>
-                            <input id="confirm-password" name="confirm-password" type="password"
-                                autocomplete="confirm-password" required
+                            <label for="password_confirmation" class="sr-only">Password</label>
+                            <input id="password_confirmation" name="password_confirmation" type="password"
+                                autocomplete="password_confirmation" required
                                 class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                 placeholder="Confirm Password">
                         </div>
