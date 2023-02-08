0e362649648b5909c4349a8e8324207e2cd033db
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-08 23:16:18 +0200

    add validation rule to check if username & email are unique

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -16,8 +17,8 @@ class RegisterController extends Controller
     {
         $fields = request()->validate([
             'name'                  => ['required', 'max:255'],
-            'username'              => ['required', 'max:255', 'min:3'],
-            'email'                 => ['required', 'max:255', 'email'],
+            'username'              => ['required', 'max:255', 'min:3', Rule::unique('users', 'username')],
+            'email'                 => ['required', 'max:255', 'email', Rule::unique('users', 'email')],
             'password'              => ['required', 'max:255', 'min:8', 'confirmed'],
             'password_confirmation' => ['required', 'max:255']
         ]);
