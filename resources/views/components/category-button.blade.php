04e43d1bf62d00211c56d0d4def301057cd3a38b
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-08 20:23:29 +0200

    update the category URL in category-button

--- resources/views/components/category-button.blade.php
+++ resources/views/components/category-button.blade.php
@@ -1,5 +1,5 @@
 @props(['category'])
 
-<a href="/categories/{{ $category->slug }}"
+<a href="/?categories={{ $category->slug }}"
     class="rounded-full border border-blue-300 px-3 py-1 text-xs font-semibold uppercase text-blue-300"
     style="font-size: 10px">{{ $category->name }}</a>
