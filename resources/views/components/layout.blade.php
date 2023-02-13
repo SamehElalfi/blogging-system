5737ce869766d67348f6a0f0f4632954a6979810
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-13 18:38:41 +0200

    add viewport meta tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
