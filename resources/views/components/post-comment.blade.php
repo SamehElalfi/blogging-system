fbd41652cb4764f5aa959dee48f40f3dd1b92ae9
Sameh Elalfi <sameh.elalfi.mail@gmail.com>
2023-02-13 20:16:55 +0200

    add user id to avatar URL to make it unique for every user

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,6 +1,6 @@
 <article class="rounded-xl border border-gray-200 bg-gray-100 p-4">
     <div class="flex items-start space-x-4">
-        <img src="https://i.pravatar.cc/100" alt="username" class="rounded-xl" />
+        <img src="https://i.pravatar.cc/100?img=user_id" alt="username" class="rounded-xl" />
         <div>
             <h3 class="text-sm">
                 <a href="/?author=username">
